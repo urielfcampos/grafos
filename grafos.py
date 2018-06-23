eb8f399d374d76e353694147d6d65be14c5b8f11
urielfcampos <urielfcampos95@gmail.com>
2018-06-23 14:45:11 -0300

    Finalizado DFS, Componentes fortementes conexos e representacao de digrafos

--- grafos.py
+++ grafos.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 class node:
     name:str
     value:int
@@ -155,11 +154,11 @@ class grafos:
                 return True
             else:
                 return False
-    def find_adj(self,edge:str,type_rep:int):
+    def find_adj(self,edge:str,type_rep:int,grafo):
         names = []
         if type_rep ==0:
-            for x in range(len(self.adjMatrix[edge])):
-                if self.adjMatrix[edge][x]== "1":
+            for x in range(len(grafo[edge])):
+                if grafo[edge][x]== "1":
                     names.append(self.find_name(x))
             return names
         if type_rep==1:
@@ -174,17 +173,24 @@ class grafos:
             for y in self.adjList:
                 if node in self.adjList[y]:
                     self.adjList[y].remove(node)
+
+
+
     def DepthFirstSearch(self,source,grafo,function_value):
         global counter_timer
         counter_timer =0
+        global connected
+        connected = []
         def visitDFS(node):
             global counter_timer
+            global  connected
             counter_timer += 1
+            connected.append(node)
             print("Visitando",node)
             color["white"].remove(node)
             color["gray"].append(node)
             time[node] =counter_timer
-            adjs = self.find_adj(node,0)
+            adjs = self.find_adj(node,0,grafo)
             for x in adjs:
                 if x in color["white"]:
                     pred[x] = node
@@ -195,6 +201,15 @@ class grafos:
             time_final[node]+= counter_timer
             print("Finalizando",node)
 
+        def iterate_paths(vertex):
+            path =[]
+            if pred[vertex][0] != None:
+                path.append(pred[vertex][0])
+                path = path+iterate_paths(pred[vertex][0])
+                print(path)
+                return path
+            else:
+                return path
         color ={"white":[],"gray":[],"black":[]}
         pred = {}
         time ={}
@@ -205,24 +220,50 @@ class grafos:
             pred.update({x:[None]})
             time.update({x:counter_timer})
             time_final.update({x:counter_timer})
-        visitDFS(source)
-        for x in grafo:
-            if x in color["white"]:
-                visitDFS(x)
-        print(time_final)
-        print(time)
-        print(color["black"])
+        if (function_value == "3"):
+            for x in grafo:
+                if x in color["white"]:
+                    visitDFS(x)
+        elif(function_value == "0"):
+           while source:
+            i = source.pop(0)
+            if i not in color["black"]:
+                visitDFS(i)
+                connected.append("")
+
+
+        #print(time_final)
+        #print(time)
+        #print(color["black"])
         finalizado = {}
-        print(pred)
+        #print(pred)
+
         for x in grafo:
             finalizado.update({x:[pred[x][0],time[x],time_final[x]]})
-        for x in finalizado:
-            print(x,finalizado[x])
+        #for x in finalizado:
+            #print(x,finalizado[x])
 
-        if (function_value == "0"):
-            return finalizado
-        else:
+        if (function_value == "5"):
+            paths =[]
+            valuesToTest =[]
+            CaminhosFinais =[]
+            #print(pred)
+            for y in pred:
+                for x in pred:
+                    if y == pred[x][0]:
+                        valuesToTest.append(y)
+
+            for x in pred:
+                if x not in valuesToTest:
+                    CaminhosFinais.append(x)
+
+            for x in CaminhosFinais:
+                paths.append(list(x)+iterate_paths(x))
+            return paths
+        elif (function_value =="3"):
             return time_final
+        else:
+            return connected
 
     def FindConnectivity(self):
         transposed=self.adjMatrix.copy()
@@ -232,26 +273,25 @@ class grafos:
         transposedmatriz = np.transpose(matriz)
         transposedmatriz = transposedmatriz.tolist()
         print(list(transposedmatriz))
-        for x in transposed:
-            for y in list(transposedmatriz):
-                transposed[x] = y[:]
-        final1 = self.DepthFirstSearch("0",self.adjMatrix,"1").copy()
+        for x,y in zip(transposed,transposedmatriz):
+            transposed[x] = y[:]
+
+        final1 = self.DepthFirstSearch("0",self.adjMatrix,"3").copy()
         stacked=[]
         for x in final1:
             stacked.append(final1[x])
-        print(stacked)
+        #print(stacked)
         stacked = sorted(stacked,reverse=True)
-        print(stacked)
+        #print(stacked)
         final2 = final1.copy()
         order_dfs = []
         for y in stacked:
             for x in final2:
                 if y == final2[x]:
                     order_dfs.append(x)
-        test_something = []
-        for x in order_dfs:
-            test_something.append(self.DepthFirstSearch(x,transposed,"0"))
-        print(test_something)
+        SCC = []
+        SCC = self.DepthFirstSearch(order_dfs,transposed,"0")
+        print("Componentes fortemente conexos :",SCC)
 
 
 
@@ -278,6 +318,6 @@ if __name__ == '__main__':
     #print(g.identify_edge(0,"0","1"))
     #g.find_adj("4",0)
     #g.addNode(0)
-    #g.DepthFirstSearch("0")
+    #g.DepthFirstSearch("0",g.adjMatrix,"0")
     g.FindConnectivity()
 
